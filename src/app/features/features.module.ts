65b54332bf01dd1380f247aa429fc4b929bd19c4
Wyatt Barnes <wyattb413@gmail.com>
2018-07-26 16:20:08 -0500

    Add Features Module

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,10 +1,30 @@
+// Angular Library
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Modules
+
+
+// Components
+import { WorkspaceComponent } from './workspace/workspace.component';
+import { GlobalHeaderComponent } from './global-header/global-header.component';
+import { SharedModule } from '../shared/shared.module';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
-  declarations: []
+  declarations: [
+    GlobalHeaderComponent,
+    WorkspaceComponent,
+    SidebarComponent
+  ],
+  exports: [
+    GlobalHeaderComponent,
+    SidebarComponent,
+    WorkspaceComponent
+  ]
 })
 export class FeaturesModule { }
